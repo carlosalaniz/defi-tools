272dc8fdeff7d0e93bf1112f5d67cc9abafa1b8c
Carlos Alaniz <carlosglvn93@gmail.com>
2021-01-13 11:55:25 -0600

    delete bin

--- bin/src/lib/common/defi/ContractEnum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ContractEnum = void 0;
-var ContractEnum;
-(function (ContractEnum) {
-    ContractEnum["Balancer"] = "balancer";
-    ContractEnum["OneInch"] = "1inch";
-})(ContractEnum = exports.ContractEnum || (exports.ContractEnum = {}));
-//# sourceMappingURL=ContractEnum.js.map
