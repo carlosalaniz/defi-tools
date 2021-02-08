4d04bbf44176e4f52fc7d35a0ddf6eba8007ae37
Carlos Alaniz <carlosglvn93@gmail.com>
2021-02-08 15:49:16 -0600

    Fix reports

--- src/tools/yield_farming/reports/AccountDataCollectorProcess.ts
+++ src/tools/yield_farming/reports/AccountDataCollectorProcess.ts
@@ -25,65 +25,63 @@ const checkEveryMilliseconds = repeatEveryMinutes * 60 * 1000; // every 2 hours
 
     async function fetchReportsAndPersist() {
         try {
-            let monitors = await MonitorUserModel.find().exec()
-            await Promise.all(monitors.map(async userMonitor => {
-                let monitorRefs = userMonitor.monitors;
-                let monitorReports = await Promise.all(monitorRefs.map(async _mid => {
-                    let monitor = await MonitorModel.findById(_mid).exec();
-                    let wallet = monitor!.walletAddress;
-                    let targetAssetAddress = monitor!.targetAssetAddress;
-                    let web3HttpConnectionString = monitor!.web3HttpConnectionString;
-                    let contractName = monitor!.contractName;
-                    let contractAddress = monitor!.contractAddress;
-                    let contractFactory = new YFSmartContractFactory(web3HttpConnectionString);
-                    let contract = contractFactory.getYFSmartContract(contractName, contractAddress);
-                    let assetsBalance = await contract.TryGetAssetsBalanceAsync(wallet);
-                    let exchangeCredentials = Array.from(monitor!.exchangeData);
-                    let accountDetails = await Promise.all(exchangeCredentials.map(async exchangeCredentials => {
-                        let _excid = exchangeCredentials.exchangeCredentials;
-                        let tradeSymbol = exchangeCredentials.tradeSymbol;
-                        let exchange = await ExchangeCredentialsModel.findById(_excid).exec();
-                        let exchangeInsterface: TradeFuturesInterface & CheckExchangeRatesInterface;
-                        switch (exchange?.exchangeName) {
-                            case ExchangeNames.BINANCE:
-                                exchangeInsterface = new BinanceExchange(exchange.apikey!, exchange.apisecret!);
-                                break;
-                            case ExchangeNames.FTX:
-                                let subaccount = (exchangeCredentials as FTXMonitorExchangeCredentials).subaccount;
-                                exchangeInsterface = new FTXExchange(exchange.apikey!, exchange.apisecret!, subaccount);
-                                break;
-                            default:
-                                throw exchange;
-                        }
-                        let accountDetails = await exchangeInsterface.tryGetAccountInformationAsync();
-                        let exchangeRate = await exchangeInsterface.tryCheckExchangeRatesInterfaceAsync(tradeSymbol, "USD");
-                        let apikey = exchange!.apikey!;
-                        return {
-                            apikey: apikey.replace(new RegExp(`^.{1,${apikey.length - 5}}`), m => "*".repeat(m.length)),
-                            exchangeName: exchange?.exchangeName,
-                            accountDetails: accountDetails,
-                            exchangeRate: {
-                                market_in: tradeSymbol,
-                                market_out: "USD",
-                                rate: exchangeRate
-                            }
-                        }
-                    }));
-                    return {
-                        _mid: monitor!._id,
-                        wallet,
-                        targetAssetAddress,
-                        contractName,
-                        assetsBalance,
-                        accountDetails
+            let monitors = await MonitorModel.find().exec()
+            let monitorReports = await Promise.all(monitors.map(async monitor => {
+                let wallet = monitor!.walletAddress;
+                let targetAssetAddress = monitor!.targetAssetAddress;
+                let web3HttpConnectionString = monitor!.web3HttpConnectionString;
+                let contractName = monitor!.contractName;
+                let contractAddress = monitor!.contractAddress;
+                let contractFactory = new YFSmartContractFactory(web3HttpConnectionString);
+                let contract = contractFactory.getYFSmartContract(contractName, contractAddress);
+                let assetsBalance = await contract.TryGetAssetsBalanceAsync(wallet);
+                let exchangeCredentials = Array.from(monitor!.exchangeData);
+                let accountDetails = await Promise.all(exchangeCredentials.map(async exchangeCredentials => {
+                    let _excid = exchangeCredentials.exchangeCredentials;
+                    let tradeSymbol = exchangeCredentials.tradeSymbol;
+                    let exchange = await ExchangeCredentialsModel.findById(_excid).exec();
+                    let exchangeInsterface: TradeFuturesInterface & CheckExchangeRatesInterface;
+                    switch (exchange?.exchangeName) {
+                        case ExchangeNames.BINANCE:
+                            exchangeInsterface = new BinanceExchange(exchange.apikey!, exchange.apisecret!);
+                            break;
+                        case ExchangeNames.FTX:
+                            let subaccount = (exchangeCredentials as FTXMonitorExchangeCredentials).subaccount;
+                            exchangeInsterface = new FTXExchange(exchange.apikey!, exchange.apisecret!, subaccount);
+                            break;
+                        default:
+                            throw exchange;
                     }
-                }))
-                await Promise.all(monitorReports.map(async monitorReport => {
+                    let accountDetails = await exchangeInsterface.tryGetAccountInformationAsync();
+                    let exchangeRate = await exchangeInsterface.tryCheckExchangeRatesInterfaceAsync(tradeSymbol, "USD");
+                    let apikey = exchange!.apikey!;
+                    return {
+                        apikey: apikey.replace(new RegExp(`^.{1,${apikey.length - 5}}`), m => "*".repeat(m.length)),
+                        exchangeName: exchange?.exchangeName,
+                        accountDetails: accountDetails,
+                        exchangeRate: {
+                            market_in: tradeSymbol,
+                            market_out: "USD",
+                            rate: exchangeRate
+                        }
+                    }
+                }));
+                return {
+                    _mid: monitor!._id,
+                    wallet,
+                    targetAssetAddress,
+                    contractName,
+                    assetsBalance,
+                    accountDetails
+                }
+            }));
+            await Promise.all(monitorReports.map(async monitorReport => {
+                if (monitorReport) {
                     let report = new MonitorReportModel()
                     report.report = monitorReport;
                     await report.save();
                     logger.log("report saved", monitorReport._mid)
-                }));
+                }
             }));
             setTimeout(fetchReportsAndPersist, checkEveryMilliseconds);
         } catch (e) {
